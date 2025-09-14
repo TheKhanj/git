c124ce70ce7e59b6b742ecf5ad7eefd85bec0bd0
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-09-14 22:29:08 +0330

    fix ssh issue

--- inc/gitsrv.bash
+++ inc/gitsrv.bash
@@ -37,11 +37,24 @@ if [ -z "$_INC_GITSRV" ]; then
 			chmod 755 "$dir"
 		fi
 
-		file="/home/git/.ssh/github"
+		file="/home/git/.ssh/id_ed25519_github"
 		if ! [ -f "$file" ]; then
 			ssh-keygen -t ed25519 -C "github" -f "$file"
 			chown git:git "$file" "${file}.pub"
+
 			echo "warning: gitsrv: don't forget to add public key to github: ${file}.pub" >&2
+		fi
+
+		file="/home/git/.ssh/config"
+		if ! [ -f "$file" ]; then
+			tee "$file" >/dev/null <<-EOF
+				Host github.com
+				    HostName github.com
+				    User git
+				    IdentityFile ~/.ssh/id_ed25519_github
+			EOF
+			chown git:git "$file"
+			chmod 600 "$file"
 		fi
 	}
 fi
