e123ba90b2d17065520afe08186615ddefaf7bf1
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-09-14 21:10:00 +0330

    add gitsrv init

--- inc/gitsrv.bash
+++ inc/gitsrv.bash
@@ -4,4 +4,14 @@ if [ -z "$_INC_GITSRV" ]; then
 	_gitsrv_list_repos() {
 		find /srv/git | grep '\.git$' | sed 's/\.git$//'
 	}
+
+	_gitsrv_init() {
+		useradd -m -s /bin/bash || true
+		su - git <<-EOF
+			cd "\$HOME"
+			mkdir -p .ssh
+			echo '$(cat conf/authorized_keys)' > .ssh/authorized_keys
+			chmod 644 .ssh/authorized_keys
+		EOF
+	}
 fi
